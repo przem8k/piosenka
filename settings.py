5c4dc04b411c2977b9262f2dad0cc88f15f25c86
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2012-06-15 21:02:13 +0200

    [design] new, thin top

--- settings.py
+++ settings.py
@@ -21,36 +21,20 @@ try:
 except ImportError:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'playground',                      # Or path to database file if using sqlite3.
-            'USER': 'playboy',                      # Not used with sqlite3.
-            'PASSWORD': 'play',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'playground',
+            'USER': 'playboy',
+            'PASSWORD': 'play',
+            'HOST': '',
+            'PORT': '',
         }
     }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'Europe/Warsaw'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+USE_I18N = False
 USE_L10N = True
 
 import os
@@ -92,9 +76,9 @@ ROOT_URLCONF = 'urls'
 FILE_CHARSET = "utf-8-sig"
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth', 
-    'django.core.context_processors.debug',  
-    'django.core.context_processors.media', 
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
@@ -115,7 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.admin',
-    'django.contrib.staticfiles',    
+    'django.contrib.staticfiles',
     'blog',
     'artists',
     'songs',
