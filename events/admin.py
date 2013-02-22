fd1ea8124687015c922629c1dcb2f9e96eb5a709
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2013-02-23 00:05:36 +0100

    [events] Amendment.

--- events/admin.py
+++ events/admin.py
@@ -2,15 +2,16 @@ from events.models import Event, Venue
 from django.contrib import admin
 
 class EventAdmin(admin.ModelAdmin):
-	prepopulated_fields = { 'slug' : ['name'] }
-	fieldsets = [
-		('What', 				{'fields': ['name', 'slug', 'artists', 'bands', 'price', 'description', 'website']}),
-		('When', 				{'fields': ['datetime',]}),
-		('Where', 				{'fields': ['venue']}),
-	]
+    prepopulated_fields = {'slug': ['name']}
+    fieldsets = [
+        ('What', {'fields': ['name', 'slug', 'artists', 'bands', 'price', 'description', 'website']}),
+        ('When', {'fields': ['datetime',]}),
+        ('Where', {'fields': ['venue']}),
+    ]
+    filter_horizontal = ['artists', 'bands',]
 
 class VenueAdmin(admin.ModelAdmin):
-	prepopulated_fields = { 'slug' : ['name', 'town']}
+    prepopulated_fields = { 'slug' : ['name', 'town']}
 	
 admin.site.register(Event, EventAdmin)
 admin.site.register(Venue, VenueAdmin)
