9125e22008b353b49875bc5e8ebe788d8b75a8c7
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2014-03-22 21:51:42 +0100

    Add template for production settings file.

---
+++ settings_production_template.py
@@ -0,0 +1,24 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+SECRET_KEY = ''
+ALLOWED_HOSTS = [
+    '.piosenkaztekstem.pl',
+]
+
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
+SERVER_EMAIL = ''
+EMAIL_USE_TLS = True
+
+S3BUCKET = ""
+GOOGLE_MAPS_API_KEY = ''
