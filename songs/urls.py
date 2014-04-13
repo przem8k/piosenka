7ba580ab636827f9baf0b88cd73ffca15e0b34d7
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2014-04-13 21:28:10 +0200

    [songs] Convert songbook views to class-based generic views.

--- songs/urls.py
+++ songs/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, url
 from django.views.generic import RedirectView
 
-from songs.views import entity, song_or_translation_entry, IndexView
+from songs.views import IndexView, ArtistView, SongView
 
 urlpatterns = patterns(
     'songs.views',
-    url(r'^(?P<slug>[-\w]+)/$', entity, name="songbook-entity"),
-    url(r'^(?P<artist_slug>[-\w]+)/(?P<song_slug>[-\w]+)/$', song_or_translation_entry,
-        name="song"),
+    url(r'^(?P<slug>[-\w]+)/$', ArtistView.as_view(), name="songbook-entity"),
+    url(r'^(?P<artist_slug>[-\w]+)/(?P<song_slug>[-\w]+)/$', SongView.as_view(), name="song"),
     # Separate print view is obsolete, redirect to song.
     url(r'^(?P<artist_slug>[-\w]+)/(?P<song_slug>[-\w]+)/drukuj/$',
         RedirectView.as_view(url='/spiewnik/%(artist_slug)s/%(song_slug)s/'), name="song-print"),
--- songs/views.py
+++ songs/views.py
@@ -1,14 +1,10 @@
 from songs.models import Song, ArtistContribution, BandContribution
 from artists.models import Artist, Band
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 from songs.lyrics import render_lyrics
-
-
-class IndexView(TemplateView):
-    template_name = "songs/index.html"
 
 
 def get_or_none(model, **kwargs):
@@ -30,66 +26,70 @@ def songs_context(request):
     return context
 
 
-def song_or_translation(request, song, template_name='songs/song.html'):
+class IndexView(TemplateView):
+    template_name = "songs/index.html"
 
-    if request.method == "GET" and "t" in request.GET:
+
+class ArtistView(TemplateView):
+    """ Lists the songs associated with the given Artist or Band object. """
+    template_name = "songs/list.html"
+
+    def get_context_data(self, **kwargs):
+        slug = kwargs['slug']
         try:
-            t = int(request.GET["t"])
-            if t >= 0 and t < 12:
-                transposition = t
-            else:
+            entity = Artist.objects.get(slug=slug)
+            songs = [x.song for x in (ArtistContribution.objects.filter(artist=entity)
+                                      .select_related('song')
+                                      .order_by('song__title'))]
+        except Artist.DoesNotExist:
+            entity = get_object_or_404(Band, slug=slug)
+            songs = [x.song for x in (BandContribution.objects.filter(band=entity)
+                                      .select_related('song')
+                                      .order_by('song__title'))]
+        context = super(ArtistView, self).get_context_data(**kwargs)
+        context['songs'] = songs
+        context['entity'] = entity
+        return context
+
+
+class SongView(TemplateView):
+    """ Displays a song. """
+    template_name = 'songs/song.html'
+
+    def get_context_data(self, **kwargs):
+        artist_slug = kwargs['artist_slug']
+        song_slug = kwargs['song_slug']
+
+        song = get_object_or_404(Song, slug=song_slug)
+        artist = get_or_none(Artist, slug=artist_slug)
+        band = get_or_none(Band, slug=artist_slug)
+
+        # verify that the song was reached via proper artist or band
+        if ((artist is None or
+             ArtistContribution.objects.filter(song=song, artist=artist).count() == 0
+             ) and
+            (band is None or
+             BandContribution.objects.filter(song=song, band=band).count() == 0)):
+            raise Http404()
+
+        if self.request.method == "GET" and "t" in self.request.GET:
+            try:
+                t = int(self.request.GET["t"])
+                if t >= 0 and t < 12:
+                    transposition = t
+                else:
+                    transposition = 0
+            except ValueError:
                 transposition = 0
-        except ValueError:
+        else:
             transposition = 0
-    else:
-        transposition = 0
+        trans_up = (transposition + 1) % 12
+        trans_down = (transposition + 11) % 12
 
-    trans_up = (transposition + 1) % 12
-    trans_down = (transposition + 11) % 12
-
-    context = {
-        'song': song,
-        'section': 'songs',
-        'trans':  transposition,
-        'trans_up': trans_up,
-        'trans_down': trans_down,
-        'lyrics': render_lyrics(song.lyrics, transposition),
-    }
-    return render(request, template_name, context)
-
-
-def song_or_translation_entry(request, artist_slug, song_slug):
-    song = get_object_or_404(Song, slug=song_slug)
-
-    artist = get_or_none(Artist, slug=artist_slug)
-    band = get_or_none(Band, slug=artist_slug)
-
-    # verify that the song was reached via proper artist or band
-    if (
-        (artist is None or
-            ArtistContribution.objects.filter(song=song, artist=artist).count() == 0) and
-        (band is None or BandContribution.objects.filter(song=song, band=band).count() == 0)
-    ):
-        raise Http404()
-
-    return song_or_translation(request, song)
-
-
-def entity(request, slug, template_name="songs/list.html"):
-    """ Lists the songs associated with the given Artist or Band object """
-    try:
-        entity = Artist.objects.get(slug=slug)
-        songs = [x.song for x in (ArtistContribution.objects.filter(artist=entity)
-                                                            .select_related('song')
-                                                            .order_by('song__title'))]
-    except Artist.DoesNotExist:
-        entity = get_object_or_404(Band, slug=slug)
-        songs = [x.song for x in (BandContribution.objects.filter(band=entity)
-                                                          .select_related('song')
-                                                          .order_by('song__title'))]
-
-    return render(request, template_name, {
-        'section': 'songs',
-        'songs': songs,
-        'entity': entity
-        })
+        context = super(SongView, self).get_context_data(**kwargs)
+        context['song'] = song
+        context['trans'] = transposition
+        context['trans_up'] = trans_up
+        context['trans_down'] = trans_down
+        context['lyrics'] = render_lyrics(song.lyrics, transposition)
+        return context
