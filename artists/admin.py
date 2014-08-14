f4c8c8aa878c46097ac6c15e74a51c81fe697c04
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2014-08-15 01:32:54 +0200

    Remove Artist and Band from admin.

--- artists/admin.py
+++ artists/admin.py
@@ -1,25 +1,8 @@
-from artists.models import Artist, Band, Entity
+from artists.models import Entity
 from django.contrib import admin
 
-
-class ArtistAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ['firstname', 'lastname']}
-    fieldsets = [
-        (None, {'fields': ['firstname', 'lastname', 'slug', 'kind', 'display']}),
-        ("Additional", {'fields': ['website']}),
-    ]
-
-
-class BandAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ['name']}
-    fieldsets = [
-        (None, {'fields': ['name', 'slug', 'members', 'display']}),
-        ("Additional", {'fields': ['website']}),
-    ]
 
 class EntityAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Artist, ArtistAdmin)
-admin.site.register(Band, BandAdmin)
 admin.site.register(Entity, EntityAdmin)
