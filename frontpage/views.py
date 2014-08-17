3dd72e08b7dc871ed3fd7202ed781ee4be638a5d
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2014-08-17 13:14:10 +0200

    Drop reliance on admin history for 'new songs' on the frontpage.

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,8 +1,6 @@
-from django.contrib.admin.models import LogEntry, ADDITION
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.views import login, logout
-from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views.generic import View, TemplateView
@@ -14,26 +12,17 @@ from events.models import Event
 from frontpage.models import CarouselItem
 from songs.models import Song
 
-def get_or_none(model, **kwargs):
-    try:
-        return model.objects.get(**kwargs)
-    except model.DoesNotExist:
-        return None
 
 class SiteIndex(TemplateView):
     template_name = "frontpage/index.html"
-    song_count = 10
+    SONG_COUNT = 10
 
     def get_context_data(self, **kwargs):
-        song_type = ContentType.objects.get(app_label="songs", model="song")
-        entries = LogEntry.objects.filter(content_type=song_type, action_flag=ADDITION).order_by("-action_time")[:SiteIndex.song_count]
-        songs = [(x.action_time, get_or_none(Song, pk=x.object_id)) for x in entries if get_or_none(Song, pk=x.object_id) != None]
-
         context = super(SiteIndex, self).get_context_data(**kwargs)
         context['carousel_items'] = CarouselItem.objects.filter(archived=False)
         context['events'] = Event.current.all()
         context['post'] = Post.objects.all().order_by('-date')[0]
-        context['songs'] = songs
+        context['songs'] = Song.objects.all().order_by('-date')[:SiteIndex.SONG_COUNT]
         return context
 
 
