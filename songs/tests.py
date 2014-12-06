27c2cd9063fbed940d409fcfa383d1e86c3a810a
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2014-12-06 21:23:03 +0100

    Make the transposition doctests visible to the test runner.

--- songs/tests.py
+++ songs/tests.py
@@ -5,7 +5,11 @@ unittest). These will both pass when you run "manage.py test".
 Replace these with more appropriate tests for your application.
 """
 
+import unittest
+import doctest
+
 from django.test import TestCase
+from songs import transpose
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -21,3 +25,6 @@ Another way to test that 1 + 1 is equal to 2.
 True
 """}
 
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(transpose))
+    return tests
