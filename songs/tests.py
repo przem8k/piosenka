aa9dae126a63f397292e03a924c0b550e0c6865c
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2014-12-07 19:09:13 +0100

    Cleanup dummy auto-created tests.

--- songs/tests.py
+++ songs/tests.py
@@ -1,29 +1,7 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
-import unittest
 import doctest
 
-from django.test import TestCase
 from songs import transpose
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(transpose))
