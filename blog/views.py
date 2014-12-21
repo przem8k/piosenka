0abc26497b9438e7c56e34a3c163f012dcf66f2b
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2014-12-21 22:58:15 +0100

    Fix edit links for blog posts.

--- blog/views.py
+++ blog/views.py
@@ -64,9 +64,9 @@ class EditPost(CheckAuthorshipMixin, UpdateView):
         date_stamp = time.strptime(year+month+day, "%Y%m%d")
         pub_date = datetime.date(*date_stamp[:3])
         return Post.objects.get(slug=slug,
-                                date__year=pub_date.year,
-                                date__month=pub_date.month,
-                                date__day=pub_date.day)
+                                pub_date__year=pub_date.year,
+                                pub_date__month=pub_date.month,
+                                pub_date__day=pub_date.day)
 
     def get_success_url(self):
         return self.object.get_absolute_url()
