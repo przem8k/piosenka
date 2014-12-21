4012c34348f3f95af5e2492fc183313f49ffb51a
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2014-12-21 22:44:55 +0100

    Update tests.

--- piosenka/test_models.py
+++ piosenka/test_models.py
@@ -8,19 +8,8 @@ class ContentItemTest(TestCase):
     def test_is_live(self):
         item = ContentItem()
 
-        item.ready = False
         item.reviewed = False
         self.assertFalse(item.is_live())
 
-        item.ready = True
-        item.reviewed = False
-        self.assertFalse(item.is_live())
-
-        # Should never happen, but let's allow that for now.
-        item.ready = False
-        item.reviewed = True
-        self.assertFalse(item.is_live())
-
-        item.ready = True
         item.reviewed = True
         self.assertTrue(item.is_live())
