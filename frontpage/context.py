a42c1bff54135ff3dd517ce396f61a432e584cd9
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2015-02-08 16:37:24 +0100

    Drop the site context processor doing nothing useful.

--- frontpage/context.py
+++
@@ -1,5 +0,0 @@
-def site_context(request):
-    url_segments = [x for x in request.path.split("/") if len(x) > 0]
-    section = url_segments[1] if len(url_segments) > 1 else None
-    context = {'section': section}
-    return context
--- piosenka/settings.py
+++ piosenka/settings.py
@@ -151,7 +151,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
-    'frontpage.context.site_context',
 )
 
 TEMPLATE_DIRS = (
