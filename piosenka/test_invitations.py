0b47bccbf8f2f0a4f3b04acf94f864138f722ddc
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2015-06-14 14:12:15 +0200

    Cleanup test_invitations.py.

--- piosenka/test_invitations.py
+++ piosenka/test_invitations.py
@@ -6,12 +6,12 @@ from piosenka.models import Invitation
 
 
 class InvitationTest(CreateUserMixin, TestCase):
-    _LOGIN_URL = reverse('hello') + '?next=' + reverse('invite')
+    _INVITE_LOGIN_URL = reverse('hello') + '?next=' + reverse('invite')
 
     def test_invite_view_get(self):
         anonymous_client = self.get_client()
         response = anonymous_client.get(reverse('invite'))
-        self.assertRedirects(response, self._LOGIN_URL)  # Redirect to login.
+        self.assertRedirects(response, self._INVITE_LOGIN_URL)
 
         regular_client = self.get_client(self.create_user())
         response = regular_client.get(reverse('invite'))
@@ -21,10 +21,10 @@ class InvitationTest(CreateUserMixin, TestCase):
         response = staff_client.get(reverse('invite'))
         self.assertEqual(200, response.status_code)
 
-    def test_invite__view_post_empty(self):
+    def test_invite_view_post_empty(self):
         anonymous_client = self.get_client()
         response = anonymous_client.post(reverse('invite'), follow=True)
-        self.assertRedirects(response, self._LOGIN_URL)  # Redirect to login.
+        self.assertRedirects(response, self._INVITE_LOGIN_URL)
 
         regular_client = self.get_client(self.create_user())
         response = regular_client.post(reverse('invite'))
@@ -34,11 +34,11 @@ class InvitationTest(CreateUserMixin, TestCase):
         response = staff_client.post(reverse('invite'))
         self.assertEqual(200, response.status_code)
 
-    def test_invite__view_post_valid(self):
+    def test_invite_view_post_valid(self):
         data = {'email_address': 'alice@example.com'}
         anonymous_client = self.get_client()
         response = anonymous_client.post(reverse('invite'), data, follow=True)
-        self.assertRedirects(response, self._LOGIN_URL)  # Redirect to login.
+        self.assertRedirects(response, self._INVITE_LOGIN_URL)
 
         regular_client = self.get_client(self.create_user())
         response = regular_client.post(reverse('invite'), data)
