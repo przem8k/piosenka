c99e8038d749f150ef07e31724aed5c717f9759e
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2015-07-14 18:52:59 +0200

    Set the unicode locale for wsgi.

--- piosenka/wsgi.py
+++ piosenka/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "piosenka.settings")
+os.environ.setdefault("LANG", "pl_PL.UTF-8")
+os.environ.setdefault("LC_ALL", "pl_PL.UTF-8")
+os.environ.setdefault("LC_LANG", "pl_PL.UTF-8")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
