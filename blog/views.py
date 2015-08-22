757e457c5720a65b84923dc0df5b40d0df4c50ca
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2015-08-22 16:50:30 +0200

    Fix 500 error in Post view when the Post does not exist.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 
 from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import get_object_or_404
@@ -12,17 +13,16 @@ from content.views import (AddContentView, EditContentView, ApproveContentView,
 
 class GetPostMixin(object):
     def get_object(self):
-        import time
         year = self.kwargs['year']
         month = self.kwargs['month']
         day = self.kwargs['day']
         slug = self.kwargs['slug']
         date_stamp = time.strptime(year+month+day, "%Y%m%d")
         pub_date = datetime.fromtimestamp(time.mktime(date_stamp))
-        return Post.objects.get(slug=slug,
-                                pub_date__year=pub_date.year,
-                                pub_date__month=pub_date.month,
-                                pub_date__day=pub_date.day)
+        return get_object_or_404(Post, slug=slug,
+                                 pub_date__year=pub_date.year,
+                                 pub_date__month=pub_date.month,
+                                 pub_date__day=pub_date.day)
 
 
 def obsolete_post(request, post_id):
