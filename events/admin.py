b53697c34cc9c70973493480d0c58fc9655c033d
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2015-09-16 23:31:53 +0200

    Add event Performer model to the admin interface.

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,4 @@
-from events.models import Event, Venue, EntityPerformance
+from events.models import Event, Venue, EntityPerformance, Performer
 from django.contrib import admin
 
 
@@ -11,8 +11,13 @@ class EventAdmin(admin.ModelAdmin):
     inlines = (EntityPerformanceInlineAdmin, )
 
 
+class PerformerAdmin(admin.ModelAdmin):
+    pass
+
+
 class VenueAdmin(admin.ModelAdmin):
     pass
 
+admin.site.register(Performer, PerformerAdmin)
 admin.site.register(Event, EventAdmin)
 admin.site.register(Venue, VenueAdmin)
