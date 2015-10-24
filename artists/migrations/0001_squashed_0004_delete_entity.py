42968f3e25aa720eb496cf843f8e5ad32ccaa42c
Przemysław Pietrzkiewicz <pietrzkiewicz@gmail.com>
2015-10-24 13:02:38 +0200

    Remove squashed migrations in artists.

--- artists/migrations/0001_squashed_0004_delete_entity.py
+++ artists/migrations/0001_squashed_0004_delete_entity.py
@@ -6,8 +6,6 @@ from django.db import models, migrations
 
 class Migration(migrations.Migration):
 
-    replaces = [('artists', '0001_initial'), ('artists', '0002_auto_20150503_1746'), ('artists', '0003_auto_20150830_1657'), ('artists', '0004_delete_entity')]
-
     dependencies = [
     ]
 
