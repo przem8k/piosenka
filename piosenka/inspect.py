02f52796b3130d7f9ce3d678a6de789f6f94958b
Przemek Pietrzkiewicz <ppi@google.com>
2020-05-25 19:59:15 +0200

    Drop old inspect locale view.

--- piosenka/inspect.py
+++ piosenka/inspect.py
@@ -7,25 +7,6 @@ from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 
-class InspectLocale(TemplateView):
-    template_name = 'inspect_locale.html'
-
-    @method_decorator(login_required)
-    @method_decorator(
-        permission_required('piosenka.inspect', raise_exception=True))
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['locale'] = str(locale.getlocale())
-        context['default_locale'] = str(locale.getdefaultlocale())
-        context['default_locale'] = str(locale.getdefaultlocale())
-        context['filesystem_encoding'] = str(sys.getfilesystemencoding())
-        context['default_encoding'] = str(sys.getdefaultencoding())
-        return context
-
-
 class InspectPermissions(TemplateView):
     template_name = 'inspect_permissions.html'
 
--- piosenka/inspect_urls.py
+++ piosenka/inspect_urls.py
@@ -4,10 +4,6 @@ import piosenka.inspect
 
 urlpatterns = [
     url(
-        r'^locale$',
-        piosenka.inspect.InspectLocale.as_view(),
-        name='inspect_locale'),
-    url(
         r'^permissions$',
         piosenka.inspect.InspectPermissions.as_view(),
         name='inspect_permissions'),
