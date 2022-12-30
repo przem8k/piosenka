c73315731d6d0ef66f0cfc5b5b1883c6ff03c1f4
Przemek Pietrzkiewicz <ppi@google.com>
2022-12-30 09:49:12 +0100

    Set DEFAULT_AUTO_FIELD, needed after Django 3.2 upgrade.
    Fixes #86 .

--- piosenka/settings.py
+++ piosenka/settings.py
@@ -181,3 +181,6 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 SITE = "https://www.piosenkaztekstem.pl"
 
 DEFAULT_FROM_EMAIL = "noreply@piosenkaztekstem.pl"
+
+# https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
