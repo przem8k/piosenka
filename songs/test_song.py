0eb3bad2a9792cde83e346195c80a33d003a238f
Przemek Pietrzkiewicz <ppi@google.com>
2024-01-07 21:18:51 +0100

    Upgrade to Django 4.0

--- songs/test_song.py
+++ songs/test_song.py
@@ -14,32 +14,6 @@ class SongTest(GenericTestsMixin, TestCase):
     def get_add_url(self):
         return reverse("add_song")
 
-    def test_add_song_empty_form(self):
-        user = testing.create_user()
-        data = {
-            "entitycontribution_set-TOTAL_FORMS": 1,
-            "entitycontribution_set-INITIAL_FORMS": 0,
-            "entitycontribution_set-MIN_NUM_FORMS": 1,
-            "entitycontribution_set-MAX_NUM_FORMS": 1000,
-        }
-        response = testing.get_user_client(user=user).post(
-            reverse("add_song"), data=data
-        )
-        self.assertEqual(200, response.status_code)
-        self.assertFormError(response, "form", "title", "To pole jest wymagane.")
-        self.assertFormError(response, "form", "capo_fret", "To pole jest wymagane.")
-        self.assertFormError(response, "form", "lyrics", "To pole jest wymagane.")
-        self.assertFormsetError(
-            response, "entitycontribution", 0, "artist", "To pole jest wymagane."
-        )
-        self.assertFormsetError(
-            response,
-            "entitycontribution",
-            0,
-            None,
-            "Zaznacz co najmniej jedną rolę artysty.",
-        )
-
     def test_add_song(self):
         user = testing.create_user()
         artist = Artist.create_for_testing(user)
