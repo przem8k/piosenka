5a8c96dcf78156f7ca1349678e84513bead9ec6e
Przemek Pietrzkiewicz <git@pnote.eu>
2025-01-31 12:40:20 +0100

    freezedry: drop more obsolete files

--- base/overrides.py
+++
@@ -1,7 +0,0 @@
-def overrides(parent_class):
-    def overrides_impl(method):
-        assert method.__name__ in dir(parent_class)
-        method.__doc__ = getattr(parent_class, method.__name__).__doc__
-        return method
-
-    return overrides_impl
--- piosenka/settings.py
+++ piosenka/settings.py
@@ -117,7 +117,6 @@ INSTALLED_APPS = (
     "easy_thumbnails",
     "compressor",
     # PzT.
-    "base",
     "piosenka",
 )
 
