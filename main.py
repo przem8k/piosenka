5bf59004ab55a351ea44cd6984bfaca6c80e226a
Przemek Pietrzkiewicz <git@pnote.eu>
2025-05-07 22:30:28 +0200

    drop obsolete comment

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from piosenka.wsgi import application
 
-# try:
-#    import googleclouddebugger
-#    googleclouddebugger.enable()
-# except ImportError:
-#    pass
-
 # App Engine by default looks for a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
 # This file imports the WSGI-compatible object of your Django app,
